50da3005bc692985a85cf9dfac9f439a215891b6
davide mezzetti <davide.mezzetti@hotmail.it>
2020-12-22 15:40:27 +0100

    add prefix api to products url

--- api/app.js
+++ api/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/products", productsRouter);
+app.use("/api/products", productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
